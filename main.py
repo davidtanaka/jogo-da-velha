e7355da52109376d3322fe09ed3741209c1c54f9
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-07-06 12:32:38 -0300

    Apenas lógica do jogo da velha

--- main.py
+++ main.py
@@ -1,4 +1,45 @@
-for i in range(0, 3):
-    print(i)
-    for j in range(0, 3):
-        print(j)
+c1 = '1'
+c2 = '2'
+c3 = '3'
+c4 = '4'
+c5 = '5'
+c6 = '6'
+c7 = '7'
+c8 = '8'
+c9 = '9'
+print(f'  {c1}  |  {c2}  |  {c3}  ')
+print(f'  {c4}  |  {c5}  |  {c6}  ')
+print(f'  {c7}  |  {c8}  |  {c9}  ')
+while True:
+
+    response_user = str(input('Aonde vc deseja colocar o x: '))
+    if response_user == c1:
+        c1 = 'X'
+        
+    if response_user == c2:
+        c2 = 'X'
+
+    if response_user == c3:
+        c3 = 'X'
+
+    if response_user == c4:
+        c4 = 'X'
+
+    if response_user == c5:
+        c5 = 'X'
+
+    if response_user == c6:
+        c6 = 'X'
+
+    if response_user == c7:
+        c7 = 'X'
+
+    if response_user == c8:
+        c8 = 'X'
+
+    if response_user == c9:
+        c9 = 'X'
+
+    print(f'  {c1}  |  {c2}  |  {c3}  ')
+    print(f'  {c4}  |  {c5}  |  {c6}  ')
+    print(f'  {c7}  |  {c8}  |  {c9}  ')
