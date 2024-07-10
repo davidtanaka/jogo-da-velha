08f622a632a785b30132cdd82d9d0931fd052f1c
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-07-10 12:52:11 -0300

    Criando meu MainWIndow

---
+++ complete-code/main_window.py
@@ -0,0 +1,18 @@
+from PySide6.QtWidgets import QMainWindow, QWidget, QGridLayout, QLabel
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
+        super().__init__(parent, *args, **kwargs)
+
+        # Configurando o básico do layout
+        self.cw = QWidget()
+        self.v_layout = QGridLayout()
+        self.cw.setLayout(self.v_layout) # type: ignore
+        self.setCentralWidget(self.cw)
+
+        # Título da janela
+        self.setWindowTitle( 'Jogo da velha ')
+
+    def adjustFixedSize(self) -> None:
+        self.adjustSize()
+        self.setFixedSize(self.width(), self.height())
